8a952f216a34a7ae3bc57e8aabcfd8de9b1e6de8
Cadu Marçal <cadubacktocode@gmail.com>
2023-06-02 13:07:21 -0300

    arrumando resultado

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-function Calculate(){
+function calculate(){
      let height = document.getElementById(`height`).value
      let weight = document.getElementById(`weight`).value
      let result = document.getElementById(`result`)
      let count = weight / (height * height)
      count = count.toFixed(2)
-     result.innerHTML = `<h4>${count}</h4>`
+     result.innerHTML = `<h4>IMC: ${count}</h4>`
 }
