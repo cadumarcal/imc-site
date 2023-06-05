9cbd3d9804d8d8ba7166ed83f59e3aee2b1fa9e1
Cadu Marçal <cadubacktocode@gmail.com>
2023-06-05 15:55:15 -0300

    arrumando js

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ function calculate(){
      let weight = document.getElementById(`weight`).value
      let result = document.getElementById(`result`)
      let count = weight / (height * height)
+     result.style.marginLeft = `65%`
+     result.style.color = `#CBD0F7;`
      count = count.toFixed(2)
-     result.innerHTML = `<h4>IMC: ${count}</h4>`
+     result.innerHTML = `<h4>IMC:${count}</h4>`
 }
